1c0030fc62c7f09d594c326028c4f9f4331ac2fe
Cylindric <mark@hanfordonline.co.uk>
2023-06-12 16:49:43 +0100

    Add some utility outputs

--- output.tf
+++ output.tf
@@ -1,8 +1,9 @@
-output netbox_vms {
-    value = data.netbox_virtual_machines.netbox_vms
-}
-
 output netbox_vm {
+    description = "The VM details retrieved from NetBox"
     value = local.netbox_vm
 }
 
+output proxmox_vm {
+    description = "The VM details created on Proxmox"
+    value = proxmox_vm_qemu.vm
+}
